347f4c8b168b0629c8323ae4b4a5395c8b160029
Robloblo <108494251+Robloblo@users.noreply.github.com>
2023-03-20 16:40:21 +0100

    quelque changement

---
+++ javascript/main.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", function() {
+    let body = document.querySelector("body");
+    var bouleDeNeige = document.createElement("div");
+    bouleDeNeige.classList = "snowBall";
+    bouleDeNeige.style.backgroundColor = "rgb(0,255,0)";
+    body.appendChild(bouleDeNeige);
+  });
+  
