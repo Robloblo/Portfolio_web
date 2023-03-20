dfe7b0fb3f95e228173aaddc95a9b3e88589531c
Robloblo <108494251+Robloblo@users.noreply.github.com>
2023-03-20 21:27:55 +0100

    quelque modif

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,9 @@
 window.addEventListener("load", function() {
-    let body = document.querySelector("body");
-    var bouleDeNeige = document.createElement("div");
-    bouleDeNeige.classList = "snowBall";
-    bouleDeNeige.style.backgroundColor = "rgb(0,255,0)";
-    body.appendChild(bouleDeNeige);
+
+   
+   
+    //Math.floor(Math.random() * 10) Utile pour faire un random entier entre 0 et 10
+        
+    
   });
   
